084288d50fcf58719025845a9868a5690ea89c17
student 8 <student 8@10.8.33.157>
2017-01-03 13:42:56 -0500

    dfghdhdfjh

---
+++ KatherineClasswork/src/gui/practice/simonGame/SimonScreenKatherine.java
@@ -0,0 +1,27 @@
+package gui.practice.simonGame;
+
+import java.util.List;
+
+import gui.components.Visible;
+import gui.sampleGames.ClickableScreen;
+
+public class SimonScreenKatherine extends ClickableScreen implements Runnable {
+
+	public SimonScreenKatherine(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void initAllObjects(List<Visible> visible) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
